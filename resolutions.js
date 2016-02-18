303f9899dd230413173b80637f434c59c172a12c
pawneetdev <pawneet.dev@gmail.com>
2016-02-18 17:00:42 +0530

    Form and event used to insert value in db

--- resolutions.js
+++ resolutions.js
@@ -7,6 +7,21 @@ if (Meteor.isClient)
             return Resolutions.find();
 		}
 	});
+
+    Template.body.events({
+        'submit .new-resolution': function(event) {
+            var title = event.target.title.value;
+
+            Resolutions.insert({
+                title: title,
+                createdAt: new Date()
+            });
+
+            event.target.title.value = "";
+
+            return false;
+        }
+    });
  }
 
 if (Meteor.isServer) {
